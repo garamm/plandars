67ca691dea982594822e8f69e87666806e149396
임가람 <garam1362@naver.com>
2020-12-10 02:35:26 +0000

    대체공휴일 추가

--- server/routes/config/db.js
+++ server/routes/config/db.js
@@ -1,8 +1,8 @@
 module.exports = {
 	connectionLimit: 10 ,
     acquireTimeout: 30000 ,  // 30 초 
-    host: '13.125.7.72',
-    port: 51021,
+    host: '13.124.65.57',
+    port: 53800,
     user: 'root',
     password: '1234',
     database: 'plandars',
--- server/routes/holiday.js
+++ server/routes/holiday.js
@@ -18,7 +18,7 @@ var request = require('request');
 const LunarCalendar = require('./util/LunarCalendar.js');
 
 
-router.get('/del', function (req, res, next) {
+router.get('/rm', function (req, res, next) {
     connection.query("DELETE FROM tb_holiday", (error, rows, fields) => {
         var jsonObj = new Object();
         if (error) {
@@ -35,9 +35,10 @@ router.get('/del', function (req, res, next) {
 
 
 
-router.get('/fixed', function (req, res, next) {
+router.get('/add', function (req, res, next) {
 	var insert = "INSERT INTO tb_holiday VALUES";
 	for(var i=2020; i<=2043; i++) {
+
 		if(i==2020) {
 			insert += "('새해', '00000001', '"+i+"-01-01', '"+i+"-01-01', '', 'Y')";
 		} else {
@@ -54,7 +55,7 @@ router.get('/fixed', function (req, res, next) {
 		
 		// 석가탄신일(음력 4월 8일) 추가
 		insert += ",('석가탄신일', '00000001', '"+i+"-"+LunarCalendar.lunarToSolar(i+'0408')+"', '"+i+"-"+LunarCalendar.lunarToSolar(i+'0408')+"', '0408', 'Y')";
-		
+
 		// 설날(음력 1월 1일과 전후 이틀) 추가
 		var newYearStr = i+"-"+LunarCalendar.lunarToSolar(i+'0101');
 		var newYearDate = new Date(newYearStr);
@@ -66,7 +67,7 @@ router.get('/fixed', function (req, res, next) {
 		
 		
 		// 추석(음력 8월 15일과 전후 이틀) 추가
-		var chuseokStr = i+"-"+LunarCalendar.lunarToSolar(i+'0101');
+		var chuseokStr = i+"-"+LunarCalendar.lunarToSolar(i+'0815');
 		var chuseokDate = new Date(chuseokStr);
 		var prevChuseokStr = LunarCalendar.getFormatDate(chuseokDate.setDate(chuseokDate.getDate() - 1));
 		var nextChuseokStr = LunarCalendar.getFormatDate(chuseokDate.setDate(chuseokDate.getDate() + 2));
@@ -75,11 +76,42 @@ router.get('/fixed', function (req, res, next) {
 		insert += ",('추석연휴', '00000001', '"+nextChuseokStr+"', '"+nextChuseokStr+"', '0815', 'Y')";
 		
 		
-		// 대체공휴일 : 연휴가 다른 공휴일과 겹치는 경우 그 날 다음의 첫 번째 비공휴일을 공휴일로 함
+		// 대체공휴일 정의
+		// 설·추석 연휴는 다른 공휴일과 겹치는 경우 그 날 다음의 첫 번째 비공휴일을 공휴일로 하고
+		// 어린이날은 토요일 또는 다른 공휴일과 겹치는 경우 그 날 다음의 첫 번째 비공휴일을 공휴일로 한다.
+		// 단, 어린이날 외의 토요일은 대체공휴일에 포함되지 않는다
+		
 		// 어린이날 대체공휴일 확인 및 추가
+		var week = ['일', '월', '화', '수', '목', '금', '토'];
+		
+		var childrensDay = new Date(i+"-05-05");
+		var childrensWeek = week[new Date('2016-07-28').getDay()];
+		if(childrensWeek == '토') {
+			childrensDay.setDate(childrensDay.getDate() + 2);
+			insert += ",('대체공휴일', '00000001', '"+LunarCalendar.getFormatDate(childrensDay)+"', '"+LunarCalendar.getFormatDate(childrensDay)+"', '', 'Y')";
+		} else if(childrensWeek == '일') {
+			childrensDay.setDate(childrensDay.getDate() + 2);
+			insert += ",('대체공휴일', '00000001', '"+LunarCalendar.getFormatDate(childrensDay)+"', '"+LunarCalendar.getFormatDate(childrensDay)+"', '', 'Y')";
+		}
+		
 		
 		// 설날 대체공휴일 확인 및 추가
-		// 추석 대체공휴일 확인 및 추가
+		var newYear1 = week[new Date(prevNewYearStr).getDay()];
+		var newYear2 = week[new Date(newYearStr).getDay()];
+		var newYear3 = week[new Date(nextNewYearStr).getDay()];
+		if(newYear1 == '일' || newYear2 == '일' || newYear3 == '일') {
+			var newYears = new Date(nextNewYearStr)
+			newYears.setDate(newYears.getDate() + 1);
+			insert += ",('대체공휴일', '00000001', '"+LunarCalendar.getFormatDate(newYears)+"', '"+LunarCalendar.getFormatDate(newYears)+"', '', 'Y')";
+		}
+		
+		// 추석 대체공휴일 확인 및 추가 (개천절이랑 겹치는지도 확인)
+		var chuseok = week[new Date(chuseokStr).getDay()];
+		if(chuseok == '일' || chuseokStr == i+'-10-03') {
+			var chuseoks = new Date(nextChuseokStr)
+			chuseoks.setDate(chuseoks.getDate() + 1);
+			insert += ",('대체공휴일', '00000001', '"+LunarCalendar.getFormatDate(chuseoks)+"', '"+LunarCalendar.getFormatDate(chuseoks)+"', '', 'Y')";
+		}
 	}
 	
     connection.query(insert, (error, rows, fields) => {
